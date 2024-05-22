f18281039579e8ca2358968b1adf4fd6df6ee95f
DriftLizard811 <omanchek@verizon.net>
2024-05-22 12:26:57 -0400

    Interactable Work
    - some work on interactable objects
    - ladder start

---
+++ Source/MyProject/InteractableObject.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableObject.generated.h"
+
+UCLASS()
+class MYPROJECT_API AInteractableObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableObject();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void Interact();
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FText interactPrompt;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
